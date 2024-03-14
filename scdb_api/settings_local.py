3bec80d6434edd40d0af05a56046b414c6843057
ys <xxjxys2021@gmail.com>
2024-03-14 15:07:42 +0800

    init project

---
+++ scdb_api/settings_local.py
@@ -0,0 +1,27 @@
+DATABASES = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'scdb',
+    'USER': 'postgres',
+    'PASSWORD': 'wAFQ63y!AEsH',
+    'HOST': 'localhost',
+    'PORT': '5432',
+}
+
+# USERTASKPATH = 'workspace/user_task'
+# ABSUSERTASKPATH = '/home/platform/phage_db/phage_api/workspace/user_task'
+# PHAGEFASTA = '/home/platform/phage_db/phage_data/data/phage_sequence/phage_fasta/'
+# PHAGEGBK = '/home/platform/phage_db/phage_data/data/phage_sequence/phage_gbk/individual_data/'
+# PHAGEGFF = '/home/platform/phage_db/phage_data/data/phage_sequence/phage_gff3/individual_data/'
+# PROTEINSEQUENCE = '/home/platform/phage_db/phage_data/data/phage_sequence/proteins/'
+# TEMPPATH = '/home/platform/phage_db/phage_data/data/tmp/'
+
+
+# CLUSTERTREEPATH = '/home/platform/phage_db/phage_data/data/phage_sequence/cluster_tree_v2/'
+# CLUSTERALIGNMENTPATH='/home/platform/phage_db/phage_data/data/analysis_data/alignment/result'
+# CLUSTERSEQUENCEPATH = '/home/platform/phage_db/phage_data/data/phage_sequence/group/'
+# METADATA = '/home/platform/phage_db/phage_api/workspace/csv_data/'
+# FASTAPATH = '/home/platform/phage_db/phage_data/data/'
+# # Analysis script path
+# ANALYSIS = '/home/platform/phage_db/phage_api/workspace/analysis_script/'
+# TASKLOG = '/home/platform/phage_db/phage_api/workspace/task_log/'
+# DEMOFILE = '/home/platform/phage_db/phage_api/demo_file/'
