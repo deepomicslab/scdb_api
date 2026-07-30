085cad443cf63d2bf1907cc95d0ef8b2c4bd9e3e
zhizhenzhou <zzz20001205@gmail.com>
2026-07-30 13:49:27 +0800

    refactor: replace if-elif dispatch chain with dict-based dispatch table
    - 178-line if-elif chain replaced by _RESULT_DISPATCH dict table
    - getresult/gettestresult reduced to 3 lines each via _dispatch() caller
    - Test mode param differences (6 cellchat/networkData) explicit in _TEST_PARAM_OMISSIONS
    - datasetInfo marked as _TEST_UNSUPPORTED
    - spider_spearman alias removed, unified to lr_spearman
    - AlphaTalk type->type_col mapping handled in _dispatch()

--- utils/analysis/scstquery_mixins/dispatch.py
+++ utils/analysis/scstquery_mixins/dispatch.py
@@ -1,178 +1,97 @@
 import pandas as pd
 from scdb_api import settings_local as local_settings
 
+_RESULT_DISPATCH = {
+    'metadata': ('getmetaresult', ['page', 'pagesize'], {}, True),
+    'organsanddatasets': ('getOrgansAndDatasets', [], {}, False),
+    'datasetInfo': ('getDatasetInfo', ['datasetPath'], {}, False),
+    'filelist': ('getdownloadfilelist', ['flag'], {}, False),
+    'scstmappingDownload': ('_scstmapping_resolve_file', ['dataset', 'method'], {}, False),
+    'download': ('download', ['filename'], {}, False),
+    'hescatter': ('getHEScatterresult', ['dataset'], {}, False),
+    'countHeatmap': ('getQueryCountHeatmapResult', ['dataset'], {}, False),
+    'hierarchicalClusteringStatus': ('getHierarchicalClusteringStatus', [], {}, False),
+    'hierarchicalclusteringCelltypes': ('getHierarchicalClusteringQueryCelltypes', ['dataset'], {}, False),
+    'hierarchicalclusteringCountHeatmap': ('getHierarchicalClusteringQueryCountHeatmapResult', ['dataset', 'cluster'], {}, False),
+    'hierarchicalClusteringMarkerGenes': ('getHierarchicalClusteringMarkerGenes', ['dataset', 'cluster'], {}, False),
+    'hierarchicalClusteringMarkerGeneExpressions': ('getHierarchicalClusteringMarkerGeneExpressions', ['dataset', 'cluster', 'gene'], {}, False),
+    'LRPairs': ('getLRPairs', ['dataset', 'mapping_method'], {}, False),
+    'LRreceivedSignalStrength': ('getReceivedSignalStrength', ['dataset', 'lr_pair', 'mapping_method'], {}, False),
+    'networkData': ('getNetworkData', ['dataset', 'type', 'mapping_method'], {}, False),
+    'img_path': ('getImgpath', ['analysis_type', 'img_id'], {}, False),
+    'cellchat_pathways': ('getCellChatPathways', ['dataset', 'mapping_method'], {}, False),
+    'cellchat_circle': ('getCellChatCircleData', ['pathway', 'dataset', 'mapping_method'], {}, False),
+    'cellchat_spatial': ('getCellChatSpatialData', ['pathway', 'dataset', 'mapping_method'], {}, False),
+    'cellchat_heatmap': ('getCellChatHeatmapData', ['LR_pair', 'dataset', 'mapping_method'], {}, False),
+    'cellchat_lrpairs': ('getCellChatLRPairs', ['dataset', 'mapping_method'], {}, False),
+    'spider_init': ('getSpiderInit', ['dataset', 'mapping_method'], {}, False),
+    'spider_pattern': ('getSpiderPatternData', ['dataset', 'pattern_id', 'mapping_method'], {}, False),
+    'spider_lr': ('getSpiderLRData', ['dataset', 'lr_name', 'mapping_method'], {}, False),
+    'lr_spearman': ('getLRSpearmanData', ['dataset', 'mapping_method'], {}, False),
+    'scstmappingStatus': ('_scstmappingStatusFallback', ['dataset'], {}, False),
+    'AlphaTalk': ('getAlphaTalkLRPairs', [], {
+        'page': 1, 'pageSize': 15, 'sender': None, 'receiver': None,
+        'ligand': None, 'sortBy': None, 'order': None, 'min_score': None,
+        'max_score': None, 'get_metadata': None, 'receptor': None,
+        'type': None, 'min_lr_score': None, 'max_lr_score': None,
+        'min_p_value': None, 'max_p_value': None, 'dataset': None,
+        'mapping_method': None,
+    }, False),
+}
+
+_TEST_PARAM_OMISSIONS = {
+    'cellchat_pathways': ['dataset'],
+    'cellchat_circle': ['dataset'],
+    'cellchat_spatial': ['dataset'],
+    'cellchat_heatmap': ['dataset'],
+    'cellchat_lrpairs': ['dataset'],
+    'networkData': ['mapping_method'],
+}
+
+_TEST_UNSUPPORTED = {'datasetInfo'}
+
 
 class DispatchMixin:
     """getresult/gettestresult dispatch methods for Scstquery."""
 
-    def getresult(self,query_params):
-        resulttype = query_params.get('resulttype')
-        if resulttype == 'metadata':
-            return self.getmetaresult(int(query_params.get('page')), int(query_params.get('pagesize')))
-        elif resulttype == 'organsanddatasets':
-            return self.getOrgansAndDatasets()
-        elif resulttype == 'datasetInfo':
-            return self.getDatasetInfo(query_params.get('datasetPath'))
-        elif resulttype == 'filelist':
-            return self.getdownloadfilelist(query_params.get('flag'))
-        elif resulttype == 'scstmappingDownload':
-            return self._scstmapping_resolve_file(query_params.get('dataset'), query_params.get('method'))
-        elif resulttype == 'download':
-            return self.download(query_params.get('filename'))
-        elif resulttype == 'hescatter':
-            return self.getHEScatterresult(query_params.get('dataset'))
-        elif resulttype == 'countHeatmap':
-            return self.getQueryCountHeatmapResult(query_params.get('dataset'))
-        elif resulttype == 'hierarchicalClusteringStatus':
-            return self.getHierarchicalClusteringStatus()
-        elif resulttype == 'hierarchicalclusteringCelltypes':
-            return self.getHierarchicalClusteringQueryCelltypes(query_params.get('dataset'))
-        elif resulttype == 'hierarchicalclusteringCountHeatmap':
-            return self.getHierarchicalClusteringQueryCountHeatmapResult(query_params.get('dataset'), query_params.get('cluster'))
-        elif resulttype == 'hierarchicalClusteringMarkerGenes':
-            return self.getHierarchicalClusteringMarkerGenes(query_params.get('dataset'), query_params.get('cluster'))
-        elif resulttype == 'hierarchicalClusteringMarkerGeneExpressions':
-            return self.getHierarchicalClusteringMarkerGeneExpressions(query_params.get('dataset'), query_params.get('cluster'), query_params.get('gene'))
-        elif resulttype == 'LRPairs':
-            return self.getLRPairs(query_params.get('dataset'), query_params.get('mapping_method'))
-        elif resulttype == 'LRreceivedSignalStrength':
-            return self.getReceivedSignalStrength(query_params.get('dataset'), query_params.get('lr_pair'), query_params.get('mapping_method'))
-        elif resulttype == 'networkData':
-            return self.getNetworkData(query_params.get('dataset'), query_params.get('type'), query_params.get('mapping_method'))
-        elif resulttype == 'img_path':
-            return self.getImgpath(query_params.get('analysis_type'), query_params.get('img_id'))
-        elif resulttype == 'cellchat_pathways':
-            return self.getCellChatPathways(query_params.get('dataset'), query_params.get('mapping_method'))
-        elif resulttype == 'cellchat_circle':
-            return self.getCellChatCircleData(query_params.get('pathway'), query_params.get('dataset'), query_params.get('mapping_method'))
-        elif resulttype == 'cellchat_spatial':
-            return self.getCellChatSpatialData(query_params.get('pathway'), query_params.get('dataset'), query_params.get('mapping_method'))
-        elif resulttype == 'cellchat_heatmap':
-            return self.getCellChatHeatmapData(query_params.get('LR_pair'), query_params.get('dataset'), query_params.get('mapping_method'))
-        elif resulttype == 'cellchat_lrpairs':
-            return self.getCellChatLRPairs(query_params.get('dataset'), query_params.get('mapping_method'))
-        elif resulttype == 'spider_init':
-            return self.getSpiderInit(query_params.get('dataset'), query_params.get('mapping_method'))
-        elif resulttype == 'spider_pattern':
-            return self.getSpiderPatternData(query_params.get('dataset'), query_params.get('pattern_id'), query_params.get('mapping_method'))
-        elif resulttype == 'spider_lr':
-            return self.getSpiderLRData(query_params.get('dataset'), query_params.get('lr_name'), query_params.get('mapping_method'))
-        elif resulttype == 'spider_spearman':
-            return self.getLRSpearmanData(query_params.get('dataset'), query_params.get('mapping_method'))
-        elif resulttype == 'lr_spearman':
-            return self.getLRSpearmanData(query_params.get('dataset'), query_params.get('mapping_method'))
-        elif resulttype == 'scstmappingStatus':
-            return self._scstmappingStatusFallback(query_params.get('dataset'))
-        elif resulttype == 'AlphaTalk':
-            return self.getAlphaTalkLRPairs(
-                page=query_params.get('page', 1),
-                pageSize=query_params.get('pageSize', 15),
-                sender=query_params.get('sender'),
-                receiver=query_params.get('receiver'),
-                ligand=query_params.get('ligand'),
-                sortBy=query_params.get('sortBy'),
-                order=query_params.get('order'),
-                min_score=query_params.get('min_score'),
-                max_score=query_params.get('max_score'),
-                get_metadata=query_params.get('get_metadata'),
-                receptor=query_params.get('receptor'),
-                type_col=query_params.get('type'),
-                min_lr_score=query_params.get('min_lr_score'),
-                max_lr_score=query_params.get('max_lr_score'),
-                min_p_value=query_params.get('min_p_value'),
-                max_p_value=query_params.get('max_p_value'),
-                dataset=query_params.get('dataset'),
-                mapping_method=query_params.get('mapping_method'),
-            )
-        else:
-            expressionfile=self.path+ '/result/scquery/sc_output_expression.csv'
-            expression = pd.read_csv(expressionfile, index_col=0)
-            return {'results': expression.to_dict(orient='records')}
+    def getresult(self, query_params):
+        return self._dispatch(query_params, is_test=False)
 
-    def gettestresult(self,query_params):
+    def gettestresult(self, query_params):
         self.path = local_settings.USERTASKPATH + 'demo_result/scst'
         self._is_demo = True
-        print(self.path)
+        return self._dispatch(query_params, is_test=True)
+
+    def _dispatch(self, query_params, is_test):
         resulttype = query_params.get('resulttype')
-        if resulttype == 'metadata':
-            return self.getmetaresult(int(query_params.get('page')), int(query_params.get('pagesize')))
-        elif resulttype == 'organsanddatasets':
-            return self.getOrgansAndDatasets()
-        elif resulttype == 'filelist':
-            return self.getdownloadfilelist(query_params.get('flag'))
-        elif resulttype == 'scstmappingDownload':
-            return self._scstmapping_resolve_file(query_params.get('dataset'), query_params.get('method'))
-        elif resulttype == 'download':
-            return self.download(query_params.get('filename'))
-        elif resulttype == 'hescatter':
-            return self.getHEScatterresult(query_params.get('dataset'))
-        elif resulttype == 'countHeatmap':
-            return self.getQueryCountHeatmapResult(query_params.get('dataset'))
-        elif resulttype == 'hierarchicalClusteringStatus':
-            return self.getHierarchicalClusteringStatus()
-        elif resulttype == 'hierarchicalclusteringCelltypes':
-            return self.getHierarchicalClusteringQueryCelltypes(query_params.get('dataset'))
-        elif resulttype == 'hierarchicalclusteringCountHeatmap':
-            return self.getHierarchicalClusteringQueryCountHeatmapResult(query_params.get('dataset'), query_params.get('cluster'))
-        elif resulttype == 'hierarchicalClusteringMarkerGenes':
-            return self.getHierarchicalClusteringMarkerGenes(query_params.get('dataset'), query_params.get('cluster'))
-        elif resulttype == 'hierarchicalClusteringMarkerGeneExpressions':
-            return self.getHierarchicalClusteringMarkerGeneExpressions(query_params.get('dataset'), query_params.get('cluster'), query_params.get('gene'))
-        elif resulttype == 'LRPairs':
-            return self.getLRPairs(query_params.get('dataset'), query_params.get('mapping_method'))
-        elif resulttype == 'LRreceivedSignalStrength':
-            return self.getReceivedSignalStrength(query_params.get('dataset'), query_params.get('lr_pair'), query_params.get('mapping_method'))
-        elif resulttype == 'cellchat_pathways':
-            return self.getCellChatPathways(mapping_method=query_params.get('mapping_method'))
-        elif resulttype == 'cellchat_circle':
-            return self.getCellChatCircleData(query_params.get('pathway'), mapping_method=query_params.get('mapping_method'))
-        elif resulttype == 'cellchat_spatial':
-            return self.getCellChatSpatialData(query_params.get('pathway'), mapping_method=query_params.get('mapping_method'))
-        elif resulttype == 'cellchat_heatmap':
-            return self.getCellChatHeatmapData(query_params.get('LR_pair'), mapping_method=query_params.get('mapping_method'))
-        elif resulttype == 'cellchat_lrpairs':
-            return self.getCellChatLRPairs(mapping_method=query_params.get('mapping_method'))
-        elif resulttype == 'spider_init':
-            # 获取元数据和坐标
-            return self.getSpiderInit(query_params.get('dataset'), query_params.get('mapping_method'))
-            
-        elif resulttype == 'spider_pattern':
-            # 获取特定 Pattern 的数值
-            return self.getSpiderPatternData(query_params.get('dataset'), query_params.get('pattern_id'), query_params.get('mapping_method'))
-            
-        elif resulttype == 'spider_lr':
-            # 获取特定 LR 的数值
-            return self.getSpiderLRData(query_params.get('dataset'), query_params.get('lr_name'), query_params.get('mapping_method'))
-        elif resulttype == 'spider_spearman':
-            return self.getLRSpearmanData(query_params.get('dataset'), query_params.get('mapping_method'))
-        elif resulttype == 'scstmappingStatus':
-            return self._scstmappingStatusFallback(query_params.get('dataset'))
-        elif resulttype == 'AlphaTalk':
-            return self.getAlphaTalkLRPairs(
-                page=query_params.get('page', 1),
-                pageSize=query_params.get('pageSize', 15),
-                sender=query_params.get('sender'),
-                receiver=query_params.get('receiver'),
-                ligand=query_params.get('ligand'),
-                sortBy=query_params.get('sortBy'),
-                order=query_params.get('order'),
-                min_score=query_params.get('min_score'),
-                max_score=query_params.get('max_score'),
-                get_metadata=query_params.get('get_metadata'),
-                receptor=query_params.get('receptor'),
-                type_col=query_params.get('type'), # 注意参数名防止冲突
-                min_lr_score=query_params.get('min_lr_score'),
-                max_lr_score=query_params.get('max_lr_score'),
-                min_p_value=query_params.get('min_p_value'),
-                max_p_value=query_params.get('max_p_value'),
-                dataset=query_params.get('dataset'),
-                mapping_method=query_params.get('mapping_method'),
-            )
-        elif resulttype == 'networkData':
-            return self.getNetworkData(query_params.get('dataset'), query_params.get('type'))
-        elif resulttype == 'img_path':
-            return self.getImgpath(query_params.get('analysis_type'), query_params.get('img_id'))
-        else:
-            expressionfile=self.path+ '/result/scquery/sc_output_expression.csv'
-            expression = pd.read_csv(expressionfile, index_col=0)
-            return {'results': expression.to_dict(orient='records')}
+        entry = _RESULT_DISPATCH.get(resulttype)
+
+        if entry is None or (is_test and resulttype in _TEST_UNSUPPORTED):
+            return self._fallback_csv()
+
+        method_name, param_names, defaults, cast_int = entry
+        method = getattr(self, method_name)
+
+        if resulttype == 'AlphaTalk':
+            kwargs = {}
+            for p, default in defaults.items():
+                val = query_params.get(p, default)
+                key = 'type_col' if p == 'type' else p
+                kwargs[key] = val
+            return method(**kwargs)
+
+        if is_test and resulttype in _TEST_PARAM_OMISSIONS:
+            param_names = [p for p in param_names if p not in _TEST_PARAM_OMISSIONS[resulttype]]
+
+        args = []
+        for p in param_names:
+            val = query_params.get(p)
+            if cast_int and val is not None:
+                val = int(val)
+            args.append(val)
+        return method(*args)
+
+    def _fallback_csv(self):
+        expressionfile = self.path + '/result/scquery/sc_output_expression.csv'
+        expression = pd.read_csv(expressionfile, index_col=0)
+        return {'results': expression.to_dict(orient='records')}
